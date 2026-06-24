9cee7238077e46f209d3e67d8cd2b284282e1673
mertdmndmn <duman.mrt@gmail.com>
2026-06-24 19:13:09 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ggg2026-v14';
+const CACHE_NAME = 'ggg2026-v15';
 self.addEventListener('install', function(e) { self.skipWaiting(); });
 self.addEventListener('activate', function(e) {
   e.waitUntil(caches.keys().then(function(keys) {
