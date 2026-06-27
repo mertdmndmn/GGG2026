aecc3b9e5aa8e97ac52f0674a1415551744552b6
mertdmndmn <duman.mrt@gmail.com>
2026-06-27 17:04:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,13 @@
-const CACHE_NAME = 'ggg2026-v28';
-self.addEventListener('install', function(e) { self.skipWaiting(); });
-self.addEventListener('activate', function(e) {
-  e.waitUntil(caches.keys().then(function(keys) {
-    return Promise.all(keys.map(function(k) { return caches.delete(k); }));
-  }).then(function() { return self.clients.claim(); }));
+const CACHE_NAME = 'ggg2026-v29';
+const urlsToCache = ['/GGG2026/', '/GGG2026/index.html'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting();
 });
-self.addEventListener('fetch', function(e) {
-  if (e.request.url.includes('supabase.co')) { e.respondWith(fetch(e.request)); return; }
-  e.respondWith(fetch(e.request).then(function(res) {
-    var clone = res.clone();
-    caches.open(CACHE_NAME).then(function(cache) { cache.put(e.request, clone); });
-    return res;
-  }).catch(function() { return caches.match(e.request); }));
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
