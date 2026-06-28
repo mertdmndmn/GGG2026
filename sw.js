f7d2c6d48b1ef3ee9679daac0a0604e93e44be27
mertdmndmn <duman.mrt@gmail.com>
2026-06-28 16:44:25 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ggg2026-v44';
+const CACHE_NAME = 'ggg2026-v45';
 const urlsToCache = ['/GGG2026/index.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
